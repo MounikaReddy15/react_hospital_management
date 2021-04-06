bb039bb8eab274b9328a1dfa8f05e9cce7f3c4d7
unknown <mounikareddygonae15@gmail.com>
2021-04-06 22:02:46 +0530

    create store

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,6 +3,29 @@ import { Input, Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const { email } = this.state;
+    if (email) {
+      // we can dispatch start login action here also
+      //   this.props.dispatch(login(email));
+    }
+  };
+
   render() {
     return (
       <div>
@@ -10,6 +33,8 @@ class Login extends Component {
           size="large"
           placeholder="Enter Email"
           prefix={<UserOutlined />}
+          onChange={this.handleEmailChange}
+          value={this.state.email}
         />
         <Button type="primary">Log In</Button>
       </div>
